5a471327c6b26276bce01c215dd0e570bfaa1f67
mab1011 <b.malika@gmail.com>
2023-03-15 01:33:31 +0300

    add comment in line 26

--- app/src/main/java/com/it381/sql3rd/MainActivity.java
+++ app/src/main/java/com/it381/sql3rd/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//comment
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
